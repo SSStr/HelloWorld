0f75105ec801746558c5cf3760ce9c7dd957f286
SSStr <whatisit@foxmail.com>
2020-04-02 02:56:02 +0800

    2:00

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World!!");
-		System.out.println("hi you!!");
+		System.out.println("hiyou!!");
 		System.out.println("come on!!");
 
 	}
